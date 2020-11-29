63d3fca4acd63bd7fa28310e0140d5311b75ddbb
Carlos César Neves Enumo <paker_wreah@hotmail.com>
2020-11-29 02:50:09 -0300

    Change SQL to query builder + decode

--- Sources/App/Repositories/UserRepository.swift
+++ Sources/App/Repositories/UserRepository.swift
@@ -2,18 +2,10 @@ import PostgresKit
 import RxSwift
 
 class UserRepository: Repository {
-    class func getAll() -> Observable<[User]> {
+    class func getAll(active: Bool? = nil) -> Observable<[User]> {
         db.withConnection { con in
-            con.query("SELECT * FROM users WHERE active = $1", [true]).flatMapEachCompactThrowing { row in
-                guard
-                    let id = row.column("id")?.int,
-                    let name = row.column("name")?.string,
-                    let email = row.column("email")?.string,
-                    let active = row.column("active")?.bool
-                    else { return nil }
-
-                return User(id: id, name: name, email: email, active: active)
-            }
+            let query = con.sql().select().columns("*").from("users")
+            return (active.map { query.where("active", .equal, $0) } ?? query).all(decoding: User.self)
         }.asObservable()
     }
 }
