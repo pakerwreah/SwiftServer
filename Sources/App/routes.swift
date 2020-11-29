7e0404acdbb82850473ff6397aef7788ad60ae4c
Carlos César Neves Enumo <paker_wreah@hotmail.com>
2020-11-29 02:49:03 -0300

    Group routes

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,15 +1,11 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
+
+    app.group("users") { group in
+        group.get(use: UserController.getUsers)
+        group.get("random", use: UserController.getRandomUsers)
+        group.get("combined", use: UserController.getCombinedUsers)
     }
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
-
-    app.get("users", use: UserController.getUsers)
-    app.get("random", use: UserController.getRandomUsers)
-    app.get("combined", use: UserController.getCombinedUsers)
 }
